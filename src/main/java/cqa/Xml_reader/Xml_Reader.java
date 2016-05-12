d5013cd046afc77c2906ba0771fb8c758009e1ba
Titas Nandi <titas.nandi01@gmail.com>
2016-05-12 16:52:09 +0200

    Xml parsing

---
+++ src/main/java/cqa/Xml_reader/Xml_Reader.java
@@ -0,0 +1,63 @@
+package cqa.Xml_reader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.XPath;
+import org.dom4j.io.SAXReader;
+public class Xml_Reader 
+{
+	public static void main(String args[])
+	{
+		File inputFile = new File("/mnt/Titas/1_QA_MODEL/SemEval_Tasks/CQA/QASelection/src/main/java/cqa/test_set_A.xml");
+		SAXReader reader = new SAXReader();
+		try {
+			Document document = reader.read(inputFile);
+			List<Node> nodes = document.selectNodes("xml/Thread");
+			System.out.println(nodes.size());
+			PrintWriter writer = null;
+			try {
+				writer = new PrintWriter(new BufferedWriter(new FileWriter("/mnt/Titas/1_QA_MODEL/SemEval_Tasks/CQA/QASelection/src/main/java/cqa/test_set_A_parsed.txt", false)));
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for(int i=0; i< nodes.size(); i++)
+			{
+				System.out.println("QUESTION:");
+				String l = nodes.get(i).selectSingleNode("RelQuestion/RelQBody").getText().trim().replaceAll("\\s+", " ");
+				System.out.println(l);
+				System.out.println("-----------------------------------------------------------------");
+				System.out.println("COMMENTS");
+			    List<Node> comment = nodes.get(i).selectNodes("RelComment");
+			    for(int j=0; j<comment.size(); j++)
+			    {
+			    	l = comment.get(j).selectSingleNode("RelCText").getText().trim().replaceAll("\\s+", " ");
+			    	System.out.println(l);
+			    	System.out.println("-----------------------------------");
+			    }
+			}
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
